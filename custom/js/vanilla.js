f01ffaf6ca78f9e03ec0a3f010439fc75d74d4b0
jihadph <38645633+jihadph@users.noreply.github.com>
2022-08-14 00:48:46 +0400

    Update Test Home 1
    Beta

--- custom/js/vanilla.js
+++ custom/js/vanilla.js
@@ -1,7 +1,8 @@
 /* jQuery Functions */
 $('document').ready(function () {
-    /* SWIPER */
-    const swiper = new Swiper('.swiper', {
+
+    /* SWIPER-FIRST */
+    const swiper_first = new Swiper('.swiper-first', {
         // Optional parameters
         direction: 'horizontal',
         loop: true,
@@ -29,7 +30,43 @@ $('document').ready(function () {
         },
     });
 
+    /* SWIPER-SECOND */
+    const swiper_second = new Swiper('.swiper-second', {
+        slidesPerView: 4,
+        spaceBetween: 30,
+        centeredSlides: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav'
+    });
+
+    $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: true,
+        centerMode: true,
+        focusOnSelect: true
+    });
+
     /* AOS */
     AOS.init();
+
+    $('.kvanar-service-panel').on('mouseover', function (){
+        let $id = $(this).attr('id');
+        $('#block-'+$id).fadeIn();
+    });
+
+    $('.kvanar-service-panel').on('mouseout', function (){
+        $('.kvanar-accordion-content').fadeOut();
+    });
 
 });
